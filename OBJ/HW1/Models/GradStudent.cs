2cc5978fdea2e6cbec109f5738aea7a1b23dd6ff
Mason McCollum <masonBmc@yahoo.com>
2018-10-24 22:01:12 -0500

    Finished Microsoft Tutorial

---
+++ OBJ/HW1/Models/GradStudent.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HW1
+{
+    class GradStudent : Student
+    {
+        public string previousDegree {get;set;}
+        public string previousUniversity {get;set;}
+        public string undergradMajor {get;set;}
+        public string undergradGPA {get;set;}
+    }
+}
---
+++ OBJ/HW1/Models/Student.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HW1
+{
+    class Student
+    {
+        public string StudentName {get;set;}
+        public DateTime DateOfBirth {get;set;}
+        public long StudentID {get;set;}
+        public string Major {get;set;}
+        public float GPA {get;set;}
+    }
+}
